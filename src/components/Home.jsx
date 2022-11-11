957b8ce2961ccaab69340cbd6430e8fe22d3d7c7
VikashKumarNokha <vikashkumarnokha@gmail.com>
2022-11-11 15:35:26 +0530

    layout completed

---
+++ src/components/Home.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import "./Home.css"
+import {  FiChevronLeft,FiChevronRight,FiChevronsLeft,FiChevronsRight } from "react-icons/fi";
+
+
+export const Home = () => {
+  return (
+    <div className='page_container'>
+
+        {/*here checkbox   */}
+       <div className='checkbox_container'>
+        <label className="container">
+        <input type="checkbox" />
+        <span className="checkmark"></span>Copy
+        </label>
+
+       <label className="container">
+        <input type="checkbox"/>
+        <span className="checkmark"></span>Move
+       </label>
+       </div>
+         {/* Here master, icons and new Element boxes   */}
+
+       <div className='main_box'>
+         {/* Box1 here */}
+           <div>
+            <p className='innerbox_header1'>Master</p>
+            <div className='innerbox_content'>
+            <p>vikash kumar</p>
+            <p>vikash kumar</p>
+            <p>vikash kumar</p>
+            <p>vikash kumar</p>
+            <p>vikash kumar</p>
+            <p>vikash kumar</p>
+            <p>vikash kumar</p>
+            <p>vikash kumar</p>
+            <p>vikash kumar</p>
+            <p>vikash kumar</p>
+            <p>vikash kumar</p>
+            <p>vikash kumar</p>
+            <p>vikash kumar</p>
+            <p>vikash kumar</p>
+            <p>vikash kumar</p>
+            <p>vikash kumar</p>
+            </div>
+           </div>
+            {/* box2 here */}
+           <div id='Icon_mainBox' >
+             <FiChevronLeft className='icon_box' />
+             <FiChevronRight className='icon_box'/>
+             <FiChevronsLeft className='icon_box'/>
+             <FiChevronsRight className='icon_box'/>
+           </div>
+            {/* Box3 here */}
+           <div>
+            <p className='innerbox_header2'>New Elements</p>
+             <div className='innerbox_content' >
+            <p>vikash kumar</p>
+            <p>vikash kumar</p>
+            <p>vikash kumar</p>
+            <p>vikash kumar</p>
+            <p>vikash kumar</p>
+            <p>vikash kumar</p>
+            <p>vikash kumar</p>
+            <p>vikash kumar</p>
+             </div>
+           </div>
+       </div>
+    </div>
+  )
+}
